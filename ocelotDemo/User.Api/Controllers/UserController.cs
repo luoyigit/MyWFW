55656924399ab78f8984811beccaffd857e1cc85
luoyi <809898030@qq.com>
2020-07-08 20:13:15 +0800

    userapi swagger 添加

---
+++ ocelotDemo/User.Api/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpGet("GetUserList")]
+        public IActionResult GetUserList()
+        {
+            return Ok("userlist");
+        }
+
+        [HttpPost("Add")]
+        public IActionResult AddUser()
+        {
+            return Ok("add");
+        }
+    }
+}
---
+++ ocelotDemo/User.Api/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+using ST.Common.Consul;
+
+namespace User.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //添加服务发现  进行配置绑定             
+            services.AddConsulClient(Configuration.GetSection("ServiceDiscovery"))
+                .AddDnsClient();
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("userApi", new OpenApiInfo() { Title = "User API 接口", Version = "v1" });
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "User.Api.xml");
+                options.IncludeXmlComments(xmlPath);
+                var bearerScheme = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Reference = new OpenApiReference()
+                    {
+                        Id = "Bearer",
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {bearerScheme,new List<string>()}
+                });
+                options.AddSecurityDefinition("Bearer", bearerScheme);
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.ShowExtensions();
+                options.SwaggerEndpoint("/swagger/userApi/swagger.json", "UserApi V1");
+            });
+            //启用服务注册于发现
+            app.UseConsul();
+        }
+    }
+}
